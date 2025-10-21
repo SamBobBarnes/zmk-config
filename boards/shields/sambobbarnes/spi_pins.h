e221316f1695420465e86249e9e86265a9509ad9
SamBobBarnes <samuelrbarnes@outlook.com>
2025-10-21 11:37:34 -0500

    add trackball back in

--- boards/shields/sambobbarnes/spi_pins.h
+++ boards/shields/sambobbarnes/spi_pins.h
@@ -1,8 +1,8 @@
-#define SPI_IO 22
-#define SPI_CLK 8
-#define SPI_CS_COL 0
-#define SPI_CS_TB 4
-#define SPI_TB_MOT 6
+#define SPI_IO      22    //P0.22
+#define SPI_CLK     8     //P0.08
+#define SPI_CS_COL  0     //P1.00
+#define SPI_CS_TB   4     //P1.04
+#define SPI_TB_MOT  6     //P0.06
 
 #define ROW_0 15 //P1.15
 #define ROW_1 13 //P1.13
