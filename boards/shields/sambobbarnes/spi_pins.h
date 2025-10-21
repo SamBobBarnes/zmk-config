6830d899f990653a0357d20cd2cbc2102c5ba443
SamBobBarnes <samuelrbarnes@outlook.com>
2025-10-21 10:53:17 -0500

    update pinout

--- boards/shields/sambobbarnes/spi_pins.h
+++ boards/shields/sambobbarnes/spi_pins.h
@@ -1,6 +1,11 @@
 #define SPI_IO 22
 #define SPI_CLK 8
 #define SPI_CS_COL 0
-#define SPI_CS_ROW 11
 #define SPI_CS_TB 4
 #define SPI_TB_MOT 6
+
+#define ROW_0 15 //P1.15
+#define ROW_1 13 //P1.13
+#define ROW_2 11 //P1.11
+#define ROW_3 10 //P0.10
+#define ROW_4 9  //P0.09
